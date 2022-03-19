86a324e0405848b6b60eba0357c104a9d0672adc
alexhook <qwertin2505@gmail.com>
2022-03-19 12:04:16 +0300

    create api.User model

---
+++ dating/api/models.py
@@ -0,0 +1,73 @@
+from django.db import models
+from django.core.mail import send_mail
+from django.core.exceptions import ValidationError
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
+
+
+GENDER_CHOICES = (
+    ('m', 'Male'),
+    ('f', 'Female'),
+)
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password):
+        user = self.model(
+            email=self.normalize_email(email),
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.model(
+            email=self.normalize_email(email),
+        )
+        user.set_password(password)
+        user.is_admin = True
+        user.is_superuser = True
+        user.is_active = True
+        user.save(using=self._db)
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    avatar = models.ImageField(upload_to='users/')
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+
+    is_active = models.BooleanField(
+        'Active', 
+        default=False,
+    )
+    is_admin = models.BooleanField(
+        'Admin', 
+        default=False,
+    )
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    EMAIL_FIELD = 'email'
+
+    def __str__(self):
+        return self.email
+    
+    def clean(self):
+        super().clean()
+        self.email = self.__class__.objects.normalize_email(self.email)
+    
+    def get_full_name(self):
+        return f'{self.first_name} {self.last_name}'
+    
+    def get_short_name(self):
+        return self.first_name
+
+    def email_user(self, subject, message, from_email=None, **kwargs):
+        """Send an email to this user."""
+        send_mail(subject, message, from_email, [self.email], **kwargs)
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        return self.is_admin
